2f89e58b625a25671897af06752d25fe4ee33fc0
Belkis Buelvas Castillo <belkis_buelvas06@hotmail.com>
2017-10-26 09:39:21 -0500

    vista

--- resources/views/autorizarRQS/index.blade.php
+++ resources/views/autorizarRQS/index.blade.php
@@ -79,8 +79,8 @@
 						<td>Belkis Buelvas</td>	
 						<td>Area</td>	
 						<td>Cargo</td>	
-						<td><a href="" title="Detalle" class="btn btn-danger glyphicon glyphicon-file btn-xs" data-title="Detalle"></a>
-							<a href="" title="Editar" class="btn btn-success glyphicon glyphicon-pencil btn-xs" data-title="Editar"></a>
+						<td><a href="" title="Detalle" class="btn btn-success glyphicon glyphicon-file btn-xs" data-title="Detalle"></a>
+							<a href="" title="Editar" class="btn btn-info glyphicon glyphicon-pencil btn-xs" data-title="Editar"></a>
 							<a href="" title="Acción" class="btn btn-primary glyphicon glyphicon-ok btn-xs" data-title="Acción"></a></td><!--
 						<td><p data-placement="top" data-toggle="tooltip" title="Eliminar"><a href="" class="btn btn-danger btn-xs" data-title="Eliminar"><span class=" glyphicon glyphicon-trash"></span></a></p></td>-->
 				
--- resources/views/entregarRQS/index.blade.php
+++ resources/views/entregarRQS/index.blade.php
@@ -81,9 +81,9 @@
 						<td>Belkis Buelvas</td>	
 						<td>Area</td>	
 						<td>Cargo</td>	
-						<td><a href="" title="Detalle" class="btn btn-danger glyphicon glyphicon-file btn-xs" data-title="Detalle"></a>
-							<a href="" title="Editar" class="btn btn-success glyphicon glyphicon-pencil btn-xs" data-title="Editar"></a>
-							<a href="" title=" Acción" class="btn btn-primary glyphicon glyphicon-ok btn-xs" data-title=" Acción"></a></td><!--
+						<td><a href="" title="Detalle" class="btn btn-success glyphicon glyphicon-file btn-xs" data-title="Detalle"></a>
+							<a href="" title="Editar" class="btn btn-info glyphicon glyphicon-pencil btn-xs" data-title="Editar"></a>
+							<a href="" title="Acción" class="btn btn-primary glyphicon glyphicon-ok btn-xs" data-title="Acción"></a></td><!--
 						<td><p data-placement="top" data-toggle="tooltip" title="Eliminar"><a href="" class="btn btn-danger btn-xs" data-title="Eliminar"><span class=" glyphicon glyphicon-trash"></span></a></p></td>-->
 				
 					</tr>                       
